0a936a3e8335dbd0a3ff3189fee6ff0ecd6bca91
nomad1712 <123312290+nomad1712@users.noreply.github.com>
2023-03-26 23:20:01 -0400

    Update User_budget.dart

--- lib/objects/User_budget.dart
+++ lib/objects/User_budget.dart
@@ -2,10 +2,11 @@ class Userbudget {
   
   final int budgetedhours;
   final String chargeCode;
-  final time endDate;
+  final Datetime endDate;
   final int spentHours;
   final String startdate;
   final String user;
+  Userbudget( this.budgetedhours , this.chargecode , this.endDate , this.spentHours ,this.startdate, this.user);
 
 
 
